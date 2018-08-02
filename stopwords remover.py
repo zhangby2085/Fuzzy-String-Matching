f7a324699b65569ab9abe323f812917ec79193bf
zhangby2085 <40630354+zhangby2085@users.noreply.github.com>
2018-08-02 14:33:32 +0300

    Add files via upload

---
+++ stopwords remover.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 14:11:47 2018
+
+@author: tibozh
+"""
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+#print stopwords.fileids()
+en_stops = set(stopwords.words('finnish'))
+all_words = open("./testrun.txt")
+for word in all_words: 
+    if word not in en_stops:
+        print(word)
