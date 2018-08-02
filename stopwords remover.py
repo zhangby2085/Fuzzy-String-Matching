cca0c8d19e665cf89d2efa867ee601fc8a23fd78
zhangby2085 <40630354+zhangby2085@users.noreply.github.com>
2018-08-02 14:59:00 +0300

    Update stopwords remover.py

--- stopwords remover.py
+++ stopwords remover.py
@@ -10,7 +10,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 #print stopwords.fileids()
 en_stops = set(stopwords.words('finnish'))
-all_words = open("./testrun.txt")
+all_words = open("./testrun.txt").read().splitlines()
 for word in all_words: 
     if word not in en_stops:
         print(word)
