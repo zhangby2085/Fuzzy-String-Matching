59b918bda209545504031e4a9226e4e57732a0a1
zhangby2085 <40630354+zhangby2085@users.noreply.github.com>
2018-08-03 11:03:02 +0300

    Update stopwords remover.py

--- stopwords remover.py
+++ stopwords remover.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug  2 14:11:47 2018
-
+#https://www.tutorialspoint.com/python/python_remove_stopwords.htm
 @author: tibozh
 """
 
@@ -9,8 +9,11 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 #print stopwords.fileids()
-en_stops = set(stopwords.words('finnish'))
+u = en_stops = set(stopwords.words('finnish'))
+U = ', '.join(u) #turn set into sr
+#all_words = open("./TXT_40.txt").read().split()
 all_words = open("./testrun.txt").read().split()
-for word in all_words: 
-    if word not in en_stops:
-        print(word)
+for word in all_words:
+    uword = word.decode('utf-8')
+    if uword not in en_stops:
+        print(uword.encode('utf-8'))
