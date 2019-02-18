66fe020957841b5b47832e01c19a6e5ac894abfe
Zhang <40630354+zhangby2085@users.noreply.github.com>
2019-02-18 12:11:44 +0200

    Update FreqneucyCount.py

--- FreqneucyCount.py
+++ FreqneucyCount.py
@@ -1,9 +1,9 @@
 import collections
 import operator
 # sample text for testing (could come from a text file)
-#text_raw = str(esal2)
+#text_raw = str(esal) #turning the select 414 files into string
 text_raw = str(bsal)
-text_replace=text_raw.replace('<bsal>','')
+text_replace=text_raw.replace('<bsal>','') #remove tag
 text=text_replace.replace('</bsal>','')
 # create a word list of the words in text 
 # remove given punctuation marks and change to lower case    
@@ -16,5 +16,5 @@ for word in word_list:
 print("The most frequent words are:")
 # operator.itemgetter(1) implies v (frequency)
 freq = operator.itemgetter(1)
-for k, v in sorted(cnt.items(), reverse=False, key=freq):
+for k, v in sorted(cnt.items(), reverse=False, key=freq): #reverse can be true, False it is easy to see from the very end of the results.
     print("%3d  %s" % (v, k))
